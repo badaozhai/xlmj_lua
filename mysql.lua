e6214c6cc134cc754d5127ab8d93b318a76063f7
chenjian <89066216@qq.com>
2016-10-18 18:16:13 +0800

    init

--- mysql.lua
+++ mysql.lua
@@ -5,13 +5,36 @@
 -- Time: 17:34
 --
 
+require "luasql.mysql"
+
+--创建环境对象
+env = luasql.mysql()
+
+--连接数据库
+conn = env:connect("数据库名","用户名","密码","IP地址","3306")
+
+--设置数据库的编码格式
+conn:execute"SET NAMES UTF8"
+
+--执行数据库操作
+cur = conn:execute("select * from role")
+
+row = cur:fetch({},"a")
+
 --文件对象的创建
-file = io.open("demo.txt","w+");
+file = io.open("role.txt","w+");
 
-var = string.format("%d %s\n", 1, "haha")
+while row do
+    var = string.format("%d %s\n", row.id, row.name)
 
-print(var)
+    print(var)
 
-file:write(var)
+    file:write(var)
+
+    row = cur:fetch(row,"a")
+end
 
 
+file:close()  --关闭文件对象
+conn:close()  --关闭数据库连接
+env:close()   --关闭数据库环境
